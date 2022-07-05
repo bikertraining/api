69438e769fd007ce7c3df73330239ad68de72648
Sammie S. Taunton <diemuzi@gmail.com>
2022-07-05 15:24:10 -0500

    Added IME, Kickstart, and Private classes Added Processing Fee and Re-Enrollement Amount

--- bikertraining_api/database/default/models/price.py
+++ bikertraining_api/database/default/models/price.py
@@ -6,6 +6,8 @@ class Price(models.Model):
     class ClassType(models.TextChoices):
         BRC = 'brc', 'Basic RiderCourse'
         ERC = 'erc', 'Experienced RiderCourse'
+        IME = 'ime', 'Kickstart'
+        PRIVATE = 'private', 'Private'
         THREEWBRC = '3wbrc', 'Three-Wheeled Basic RiderCourse'
 
     amount = models.DecimalField(
@@ -19,12 +21,28 @@ class Price(models.Model):
     class_type = models.CharField(
         blank=False,
         choices=ClassType.choices,
-        max_length=5,
+        max_length=7,
         null=False
     )
 
     is_active = models.BooleanField(
         default=True
+    )
+
+    process_amount = models.DecimalField(
+        blank=False,
+        decimal_places=2,
+        default=0.00,
+        max_digits=10,
+        null=False
+    )
+
+    re_amount = models.DecimalField(
+        blank=False,
+        decimal_places=2,
+        default=0.00,
+        max_digits=10,
+        null=False
     )
 
     tracker = FieldTracker()
