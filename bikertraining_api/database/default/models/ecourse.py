b81bdba855071670e495ae09b7fb8ca75f2c1cdc
Sammie S. Taunton <diemuzi@gmail.com>
2023-02-17 09:39:43 -0600

    New e3/e5 links

--- bikertraining_api/database/default/models/ecourse.py
+++ bikertraining_api/database/default/models/ecourse.py
@@ -3,7 +3,12 @@ from model_utils import FieldTracker
 
 
 class Ecourse(models.Model):
-    link_brc = models.URLField(
+    link_brc_e3 = models.URLField(
+        blank=False,
+        null=False
+    )
+
+    link_brc_e5 = models.URLField(
         blank=False,
         null=False
     )
